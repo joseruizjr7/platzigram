5c58bcd193119a450ac5481d8d7a3170e6e959a3
Jose Ruiz <joseruizjr7160@gmail.com>
2020-02-06 17:06:53 -0600

    a new field was added in users

--- posts/models.py
+++ posts/models.py
@@ -8,11 +8,13 @@ class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    is_admin = models.BooleanField(default=False)
+
     bio = models.TextField(blank=True)
 
     birthdate = models.DateField(blank=True, null=True)
 
     created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField()
+    modified = models.DateTimeField(auto_now=True)
 
 
