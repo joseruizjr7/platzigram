f79f668a32a24d4a1d391f3275c3978bbccc467e
Jose <joseruiz.dev@gmail.com>
2020-02-10 19:24:04 -0600

    Profile model registered in the admin + .gitignore

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user', 'phone_number')
+    search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name', 'phone_number')
+    list_filter = ('created', 'modified')
