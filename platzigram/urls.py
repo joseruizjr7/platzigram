f7eda25db72c9fa447b19ca648755a4fab667f91
Jose <joseruiz.dev@gmail.com>
2020-02-15 14:19:19 -0600

    post model created

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-# from platzigram import views as local_views
-# from posts import views as posts_views
+
+
+from platzigram import views as local_views
+from posts import views as posts_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'photo',
+        'user',
+        'created',
+        'modified',
+    )
+
+    list_display_links = (
+        'title',
+    )
+
+    list_filter = (
+        'created',
+        'modified',
+    )
+
+    fieldsets = (
+        ('User', {
+            'fields': (('user', 'profile'),),
+        }),
+        ('Content', {
+            'fields': (
+                ('title'),
+                ('photo'),
+            ),
+        }),
+    )
+
